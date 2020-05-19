0b40f7d0c402e6153dc6cea610e7b5c68e836e0f
Juan Camilo <jc.castiblancor@uniandes.edu.co>
2020-05-19 15:10:50 -0500

    25

--- src/main/live_audio_processing.py
+++ src/main/live_audio_processing.py
@@ -16,7 +16,7 @@ rutaPesos = "./src/modelo/" + pesos + ".h5"
 
 SAMPLE_RATE = 22050
 window_length_stft_mfcc = 0.032
-window_length_stft_esp = 0.032
+window_length_stft_esp = 0.025
 Step_size_stft = 0.025
 ventana_Tiempo_ = 0.100
 INPUT_FRAMES_PER_BLOCK = int(SAMPLE_RATE * ventana_Tiempo_)
